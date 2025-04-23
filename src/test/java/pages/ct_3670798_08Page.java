7bf08cf732811db079890bf4d7e0ae94e8b64931
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-22 18:14:09 -0600

    agregando src/test/java/pages/ct_3670798_08Page.java

---
+++ src/test/java/pages/ct_3670798_08Page.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ct_3670798_08Page {
+    WebDriver driver;
+    
+    public ct_3670798_08Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void locatePreRequestColumn() {
+        // Implementación para ubicar la columna 'Folio Pre solicitud'
+    }
+
+    public void verifyPreRequestColumnPosition() {
+        // Implementación para verificar la posición de la columna 'Folio Pre solicitud'
+    }
+
+    public void locatePaymentFolioColumn() {
+        // Implementación para ubicar la columna 'Folio de Pago'
+    }
+
+    public void verifyPaymentFolioColumnPosition() {
+        // Implementación para verificar la posición de la columna 'Folio de Pago'
+    }
+
+    public void verifyColumnsAreEmpty() {
+        // Implementación para confirmar que las columnas estén vacías
+    }
+}
